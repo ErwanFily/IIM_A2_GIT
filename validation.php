9f69b806659d0cc8e870df22f3b336bd0adbbeed
FaroukGad <farouk5.gad@gmail.com>
2016-09-15 13:54:58 +0200

    début validation

--- validation.php
+++ validation.php
@@ -6,7 +6,13 @@ session_start();
 if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
 	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
-	// TODO
+	
+
+	 $username = htmlspecialchars($_POST["username"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $password = htmlspecialchars($_POST["password"])
+
+    
 
 }else{ 
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
